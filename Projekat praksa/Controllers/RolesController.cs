926d8e1c55936b3d06568dd564c0de6812ce6329
IrinejIT52 <79905914+IrinejIT52@users.noreply.github.com>
2025-01-07 20:56:51 +0100

    Added roles and function to give roles, such as user, admin and owner

--- Projekat praksa/Controllers/RolesController.cs
+++ Projekat praksa/Controllers/RolesController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Projekat_praksa.Constants;
+using Projekat_praksa.Database;
+
 
 namespace Projekat_praksa.Controllers
 {
@@ -7,7 +12,44 @@ namespace Projekat_praksa.Controllers
 	[ApiController]
 	public class RolesController : ControllerBase
 	{
+		private readonly UserRepository _userRepository;
 
+		public RolesController(UserRepository userRepository)
+		{
+			_userRepository = userRepository;	
+		}
+
+		[HttpPost("addRoles")]
+		public ActionResult AddRole(IdentityRole role)
+		{
+			_userRepository.AddRole(role);
+
+			return Ok();
+		}
+
+		[HttpPost("user/{userid}/role/{roleid}")]
+		public ActionResult AddUserRole(string userid, string roleid)
+		{
+			_userRepository.AddUserRole(new IdentityUserRole<string> { RoleId=roleid, UserId=userid});
+
+			return Ok();
+		}
+
+		[HttpPost("user/{userid}/admin")]
+		public ActionResult AddUserRoleAdmin(string userid)
+		{
+			_userRepository.AddUserRole(new IdentityUserRole<string> { RoleId = Constants.Constants.adminId , UserId = userid });
+
+			return Ok();
+		}
+
+		[HttpPost("user/{userid}/user")]
+		public ActionResult AddUserRoleUser(string userid)
+		{
+			_userRepository.AddUserRole(new IdentityUserRole<string> { RoleId = Constants.Constants.userId, UserId = userid });
+
+			return Ok();
+		}
 
 	}
 }
