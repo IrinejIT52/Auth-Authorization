ea5cb72c5e2ed6b825a34d4ad0ab9aae86fa2c35
IrinejIT52 <79905914+IrinejIT52@users.noreply.github.com>
2025-01-09 13:12:51 +0100

    adding role endpoints

--- Projekat praksa/Controllers/RolesController.cs
+++ Projekat praksa/Controllers/RolesController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using Projekat_praksa.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Projekat_praksa.Constants;
 using Projekat_praksa.Database;
 
 
@@ -12,6 +17,33 @@ namespace Projekat_praksa.Controllers
 	[ApiController]
 	public class RolesController : ControllerBase
 	{
+		private readonly UserRepository _userRepository;
+
+		public RolesController(UserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+		[HttpPost("addRoles")]
+		public ActionResult AddRole(IdentityRole role)
+		{
+			_userRepository.AddRole(role);
+
+			return Ok();
+		}
+
+		[HttpPost("user/{userid}/role/{roleid}")]
+		public ActionResult AddUserRole(string userid, string roleid)
+		{
+			_userRepository.AddUserRole(new IdentityUserRole<string> { RoleId = roleid, UserId = userid });
+
+			return Ok();
+		}
+
+		[HttpPost("user/{userid}/admin")]
+		public ActionResult AddUserRoleAdmin(string userid)
+		{
+			_userRepository.AddUserRole(new IdentityUserRole<string> { RoleId = Constants.Constants.adminId, UserId = userid });
 
 			return Ok();
 		}
